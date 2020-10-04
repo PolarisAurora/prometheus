68259543617e60f9eb0d88eea280b0753f67e8cf
Ternura <843648308@qq.com>
2020-10-04 13:30:07 +0800

    国际化

---
+++ src/main/java/com/telnet/prometheus/configuration/SMSServiceConfiguration.java
@@ -0,0 +1,8 @@
+package com.telnet.prometheus.configuration;
+
+/**
+ * @author Ternura
+ * @since 2020/10/3 23:15
+ */
+public class SMSServiceConfiguration {
+}
---
+++ src/main/java/com/telnet/prometheus/controller/SMSController.java
@@ -0,0 +1,9 @@
+package com.telnet.prometheus.controller;
+
+/**
+ * 短信服务
+ * @author Ternura
+ * @since 2020/10/3 21:50
+ */
+public class SMSController {
+}
